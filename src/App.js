e5a1259fe96040c67df9caae8be40740c26728c3
Fahad Ali Khan <fk99970@gmail.com>
2021-04-11 15:39:09 +0500

    final Working app from scratch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Survey } from 'survey-react';
-// import Survey from 'survey-react';
-
+import Question from './components/Question';
 import './App.css';
 
 const options = 'Not at all,Several days,More than half the days,Nearly every day'.split(
 	','
 );
+
+const answers = [];
 
 const optionsToScore = {
 	'Not at all': 0,
@@ -15,162 +15,92 @@ const optionsToScore = {
 	'Nearly every day': 3,
 };
 
-var surveyJSON = {
-	title: 'Survey App Demo',
-	pages: [
-		{
-			name: 'page1',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 1',
-					title: 'Little interest or pleasure in doing things',
-				},
-			],
-		},
-		{
-			name: 'page2',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 2',
-					title: 'Feeling down, depressed, or hopeless',
-				},
-			],
-		},
-		{
-			name: 'page3',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 3',
-					title: 'Trouble falling or staying asleep, or sleeping too much',
-				},
-			],
-		},
-		{
-			name: 'page4',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 4',
-					title: 'Feeling tired or having little energy',
-				},
-			],
-		},
-		{
-			name: 'page5',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 5',
-					title: 'Poor appetite or overeating',
-				},
-			],
-		},
-		{
-			name: 'page6',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 6',
-					title:
-						'Feeling bad about yourself — or that you are a failure or have let yourself or your family down ',
-				},
-			],
-		},
-		{
-			name: 'page7',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 7',
-					title:
-						'Trouble concentrating on things, such as reading the newspaper or watching television',
-				},
-			],
-		},
-		{
-			name: 'page8',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 8',
-					title:
-						'Moving or speaking so slowly that other people could have noticed? Or the opposite — being so fidgety or restless that you have been moving around a lot more than usual',
-				},
-			],
-		},
-		{
-			name: 'page9',
-			questions: [
-				{
-					type: 'radiogroup',
-					choices: options,
-					isRequired: true,
-					name: 'question 9',
-					title:
-						'Thoughts that you would be better off dead or of hurting yourself in some way',
-				},
-			],
-		},
-	],
-};
-
 const questions = [
 	{
-		title: 'title of question 1',
+		title: '1. Little interest or pleasure in doing things',
 		options: options,
 	},
 	{
-		title: 'title of question 2',
+		title: '2. Feeling down, depressed, or hopeless',
 		options: options,
 	},
 	{
-		title: 'title of question 3',
+		title: '3. Trouble falling or staying asleep, or sleeping too much',
+		options: options,
+	},
+	{
+		title: '4. Feeling tired or having little energy',
+		options: options,
+	},
+	{
+		title: '5. Poor appetite or overeating',
+		options: options,
+	},
+	{
+		title:
+			'6. Feeling bad about yourself - or that you are a failure or have let yourself or your family down',
+		options: options,
+	},
+	{
+		title:
+			'7. Trouble concentrating on things, such as reading the newspaper or watching television',
+		options: options,
+	},
+	{
+		title:
+			'8. Moving or speaking so slowly that other people could have noticed Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual',
+		options: options,
+	},
+	{
+		title:
+			'9. Thoughts that you would be better off dead, or of hurting yourself',
+		options: options,
+	},
+	{
+		title:
+			'10. If you checked off any problems, how difficult have these problems made it for you at work, home, or with other people?',
 		options: options,
 	},
 ];
 
 class App extends Component {
-	sendDataToServer = (survey) => {
+	state = {
+		showTotal: false,
+		totalScore: 0,
+	};
+	clickedHandler = (event) => {
 		const scores = [];
-		// console.log('sending data to server');
-		// console.log(survey.data);
-		for (const key in survey.data) {
-			// console.log(survey.data[key]);
-			// console.log(typeof survey.data[key]);
-			// console.log(optionsToScore['Not at all']);
-			// console.log(optionsToScore[survey.data[key]]);
-			scores.push(optionsToScore[survey.data[key]]);
+		answers.push(event.target.textContent);
+		console.log(answers);
+		for (const answer of answers) {
+			scores.push(optionsToScore[answer]);
 		}
-		console.log(scores);
 		const sum = scores.reduce(
 			(prevValue, currentValue) => (prevValue += currentValue),
 			0
 		);
-		console.log(sum);
+		this.totalScore = sum;
+	};
+	showTotalScore = () => {
+		this.setState({ showTotal: true });
 	};
 	render() {
 		return (
 			<div className='App'>
-				<Survey json={surveyJSON} onComplete={this.sendDataToServer} />
+				{questions.map((question) => (
+					<Question
+						key={Math.random()}
+						title={question.title}
+						options={question.options}
+						clicked={this.clickedHandler}
+					/>
+				))}
+				{this.state.showTotal ? (
+					<h1>Your Total Score is: {this.totalScore}</h1>
+				) : null}
+				<button className='next-btn' onClick={this.showTotalScore}>
+					Next
+				</button>
 			</div>
 		);
 	}
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
+import './Question.css';
+
+const clickHandler = (event) => {
+	if (event.target.className === 'Selected') {
+		event.target.className = 'Options-Item';
+	} else {
+		event.target.className = 'Selected';
+	}
+	console.log(event.target.textContent);
+};
+
 const question = (props) => {
 	return (
-		<div>
-			<label>{props.title}</label>
-			<div>
-				<ul>
+		<div className='Question'>
+			<label className='Label'>{props.title}</label>
+			<div className='Options-Container'>
+				<ul className='Options-List'>
 					{props.options.map((option) => (
-						<li>{option}</li>
+						<li
+							className='Options-Item'
+							key={Math.random()}
+							onClick={(event) => {
+								clickHandler(event);
+								props.clicked(event);
+							}}>
+							{option}
+						</li>
 					))}
 				</ul>
 			</div>
